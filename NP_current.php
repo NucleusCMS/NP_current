3bda2a1a4f0e7a99b6ca78397677a9f970087617
yama <yamamoto@kyms.jp>
2018-08-01 15:19:36 +0900

    Update

--- NP_current.php
+++ NP_current.php
@@ -4,7 +4,7 @@ class NP_current extends NucleusPlugin
 	var $table;
 	function getName()        { return 'NP_current';}
 	function getAuthor()      { return 'yama';}
-	function getURL()         { return 'http://kyms.ne.jp/';}
+	function getURL()         { return 'http://kyms.jp/';}
 	function getVersion()     { return '0.1';}
 	function getDescription() { return 'NP_current';}
 	function supportsFeature($w) { return ($w=='SqlTablePrefix') ? 1 : 0 ; }

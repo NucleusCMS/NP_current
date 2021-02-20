c297e322528368946cc4ff3865f5a343b4237a54
yama <yamamoto@kyms.jp>
2021-02-20 17:05:20 +0900

    refactor

--- NP_current.php
+++ NP_current.php
@@ -1,22 +1,19 @@
 <?php
 class NP_current extends NucleusPlugin
 {
-	var $table;
-	function getName()        { return 'NP_current';}
-	function getAuthor()      { return 'yama';}
-	function getURL()         { return 'http://kyms.jp/';}
-	function getVersion()     { return '0.1';}
-	function getDescription() { return 'NP_current';}
-	function supportsFeature($w) { return ($w=='SqlTablePrefix') ? 1 : 0 ; }
-	
-	function doTemplateVar (&$item)
-	{
-		global $itemid;
-		$args = func_get_args();
-		array_splice($args, 0, 1);
-		$str = join(',', $args);
-		$result = (intval($itemid) == intval($item->itemid)) ? $str : '';
-		echo $result;
-	}
+    public function getName()        { return 'NP_current';}
+    public function getAuthor()      { return 'yama';}
+    public function getURL()         { return 'https://kyms.jp/';}
+    public function getVersion()     { return '0.1';}
+    public function getDescription() { return 'NP_current';}
+    public function supportsFeature($w) { return ($w === 'SqlTablePrefix') ? 1 : 0 ; }
+    public function doTemplateVar (&$item)
+    {
+        global $itemid;
+        if ((int)$itemid !== (int)$item->itemid) {
+            return '';
+        }
+        $args = func_get_args();
+        return implode(',', array_splice($args, 0, 1));
+    }
 }
-?>
